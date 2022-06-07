b286b66d729b880090946c6238456543b205e0ca
ErickBertoti <95359762+ErickBertoti@users.noreply.github.com>
2022-06-07 12:11:06 -0300

    Add files via upload

---
+++ app noticia/appnoticia/app/src/main/java/com/example/appnoticia/noticia.java
@@ -0,0 +1,22 @@
+package com.example.appnoticia;
+
+public class noticia {
+
+    private String img;
+    private String titulo;
+    private String data;
+    private String texto;
+    private String autor;
+
+    public noticia(){}
+    public noticia(String i, String t, String d, String te, String a){
+        img = i;
+        titulo = t;
+        data = d;
+        texto = te;
+        autor = a;
+
+
+        }
+    }
+
